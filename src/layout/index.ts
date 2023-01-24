e8ed72acb43bd246b50b27323c47512f08ded696
Omurbek <firefoxer00@gmail.com>
2023-01-24 12:32:51 +0600

    add form in header

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -4,7 +4,6 @@
 import Header from "./header/Header"
 import Footer from "./footer/Footer"
 import Layout from "./layout/Layout"
-import Aside from "./aside/Aside"
 import Wrapper from "./wrapper/Wrapper"
 
-export { Wrapper, Header, Footer, Layout, Aside }
+export { Wrapper, Header, Footer, Layout }
--- src/layout/layout/Layout.tsx
+++ src/layout/layout/Layout.tsx
@@ -2,14 +2,12 @@ import { FC } from "react"
 import { LayoutProps } from "./Layout.props"
 import Header from "../header/Header"
 import Footer from "../footer/Footer"
-import Aside from "../aside/Aside"
 import Wrapper from "../wrapper/Wrapper"
 
 const Layout: FC<LayoutProps> = ({ children }): JSX.Element => {
   return (
     <>
       <Header />
-      <Aside />
       <Wrapper>{children}</Wrapper>
       <Footer />
     </>
